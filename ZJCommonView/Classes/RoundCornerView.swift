5006c5b7307f8f853a7b978061705c56267b3006
yonathan <yonathan@asetku.com>
2023-08-25 16:12:37 +0800

    optimize

--- ZJCommonView/Classes/RoundCornerView.swift
+++ ZJCommonView/Classes/RoundCornerView.swift
@@ -9,9 +9,22 @@ import UIKit
 
 public class RoundCornerView: UIView {
     
+    private var borderLayer: CAShapeLayer?
+    
+    public struct BorderInfo {
+        let width: CGFloat
+        let color: UIColor
+    }
+    
     let radius: CGFloat
     
-    public var corners: UIRectCorner? {
+    public var border: BorderInfo? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    public var corners: UIRectCorner = .allCorners {
         didSet {
             setNeedsDisplay()
         }
@@ -20,30 +33,50 @@ public class RoundCornerView: UIView {
     public init(radius: CGFloat) {
         self.radius = radius
         super.init(frame: .zero)
+        
         backgroundColor = .white
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     public override func layoutSubviews() {
         super.layoutSubviews()
-        guard let rectCorner = corners else { return }
-        addRoundCorners(rect: bounds, corners: rectCorner, radius: radius)
+        
+        addRoundCornersBy(corners: corners, radius: radius)
+        
+        addBorder(border)
+        
     }
     
-    private func addRoundCorners(rect: CGRect, corners: UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: .init(width: radius, height: radius))
+    private func addRoundCornersBy(corners: UIRectCorner, radius: CGFloat) {
+        
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: .init(width: radius, height: radius))
         
         let maskLayer = CAShapeLayer()
         maskLayer.frame = bounds
         maskLayer.path = path.cgPath
-
+        
         layer.mask = maskLayer
+        
     }
     
+    private func addBorder(_ border: BorderInfo?) {
+        
+        borderLayer?.removeFromSuperlayer()
+        
+        guard let border = border else { return }
+        
+        let maskPathLine = UIBezierPath.init(roundedRect: bounds, cornerRadius: radius)
+        borderLayer = CAShapeLayer.init()
+        borderLayer?.frame = bounds
+        borderLayer?.path = maskPathLine.cgPath
+        borderLayer?.strokeColor = border.color.cgColor
+        borderLayer?.lineWidth = border.width
+        borderLayer?.fillColor = nil
+        layer.addSublayer(borderLayer!)
+       
+   }
+    
 }
-
-
-
